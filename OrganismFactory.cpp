d077c2d67821512e50f149f417350fd5dfb9b103
FFugi <fudifil@gmail.com>
2018-04-22 16:55:03 +0200

    add OrganismFactory

---
+++ OrganismFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ffugi on 22.04.18.
+//
+
+#include "AnimalFactory.hpp"
---
+++ OrganismFactory.hpp
@@ -0,0 +1,14 @@
+//
+// Created by ffugi on 22.04.18.
+//
+
+#ifndef VIRTUAL_WORLD_1_ANIMALFACTORY_HPP
+#define VIRTUAL_WORLD_1_ANIMALFACTORY_HPP
+
+
+class AnimalFactory {
+
+};
+
+
+#endif //VIRTUAL_WORLD_1_ANIMALFACTORY_HPP
